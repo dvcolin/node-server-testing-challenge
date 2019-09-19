8f3ec6d5e8abbb09ed8f4089e225caa80803d5a9
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-19 10:30:49 -0700

    moved tests for tracks-model into own file

--- api/routes/tracks-router.js
+++ api/routes/tracks-router.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
 const router = express.Router();
+const Tracks = require('../models/tracks-model.js');
 
+router.post('/tracks', (req, res) => {
+    const track = req.body;
 
+    Tracks.insert(track)
+    .then(res => {
+        return res;
+    })
+})
 
 
 
--- api/server.test.js
+++ api/server.test.js
@@ -1,9 +1,6 @@
 const request = require('supertest');
 const server = require('./server.js');
 
-const db = require('../data/db-config.js');
-const Tracks = require('./models/tracks-model.js');
-
 describe('server.js', () => {
     describe('index route', () => {
         it('should return OK status code', () => {
@@ -27,66 +24,6 @@ describe('server.js', () => {
                 response = res;
 
                 expect(response.type).toEqual(expectedResponseType);
-            })
-        })
-    });
-
-    describe('tracks route', () => {
-        describe('insert()', () => {
-            beforeEach(async () => {
-                // this function executes and clears out the table before each test
-                await db('tracks').truncate();
-              });
-              
-
-            it('should be able to post tracks', async () => {
-                await Tracks.insert({ "track_name": "Tripolar Dub", "track_artist": "Akeos" })
-                await Tracks.insert({ "track_name": "Wonton Bass", "track_artist": "Aweminus" })
-    
-                const tracks = await db('tracks');
-    
-                expect(tracks).toHaveLength(2);
-            });
-
-            it('should be able to post a new track', async () => {
-                const track = await Tracks.insert({ track_name: 'Bad Sectors (Metahumper VIP)', track_artist: 'Mighty Duck & KC Dubz' })
-
-                expect(track.track_name).toEqual('Bad Sectors (Metahumper VIP)')
-
-            })
-        });
-
-        describe('remove()', () => {
-            beforeEach(async () => {
-                // this function executes and clears out the table before each test
-                await db('tracks').truncate();
-              });
-
-            it('should be able to delete a track', async () => {
-                await Tracks.insert({ "track_name": "Tripolar Dub", "track_artist": "Akeos" })
-                await Tracks.insert({ "track_name": "Wonton Bass", "track_artist": "Aweminus" })
-    
-                let tracks = await db('tracks');
-                expect(tracks).toHaveLength(2);
-
-                await Tracks.remove(1)
-                tracks = await db('tracks');
-                expect(tracks).toHaveLength(1);
-            
-            });
-
-            it('should delete the track with the specified id', async () => {
-                await Tracks.insert({ "track_name": "Tripolar Dub", "track_artist": "Akeos" })
-                await Tracks.insert({ "track_name": "Wonton Bass", "track_artist": "Aweminus" })
-    
-                let tracks = await db('tracks');
-                expect(tracks).toHaveLength(2);
-
-                await Tracks.remove(2)
-
-                const response = await Tracks.getById(2)
-
-                expect(response).toEqual({ error: 'Track already deleted!' });
             })
         })
     });
